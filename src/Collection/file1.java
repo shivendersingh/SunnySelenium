6410ad1033fbc7ec39bf6b67a249623c6e12011b
Shivender singh <shivender339@gmail.com>
2024-10-10 00:18:37 +0530

    Create file1.java

---
+++ src/Collection/file1.java
@@ -0,0 +1,13 @@
+public void login()
+	{
+		System.out.println("Remotely change User able to login......");
+	}
+	public void logout()
+	{
+		System.out.println("Remotely change User able to logout......");
+	}
+	public static void main(String[] args) {
+			System.out.println(" Remotely Hellow worlds");
+	
+		}
+	}
