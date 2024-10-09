6b98201501ae2bfb0a1422a1f701520f72a28651
Shivender singh <shivender339@gmail.com>
2024-10-10 00:06:25 +0530

    creating the new branch for learning purpose

---
+++ src/Collection/newfile.java
@@ -0,0 +1,12 @@
+package Collection;
+public class newfile {
+
+	public void login()
+	{
+		System.out.println("User able to login......");
+	}
+	public static void main(String[] args) {
+			System.out.println("Hellow worlds");
+	
+		}
+	}
