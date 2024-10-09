3bcd8a65e278621bf678a421e6c82c439f7653a4
Shivender singh <shivender339@gmail.com>
2024-10-10 00:09:41 +0530

    Update newfile.java

--- src/Collection/newfile.java
+++ src/Collection/newfile.java
@@ -3,7 +3,11 @@ public class newfile {
 
 	public void login()
 	{
-		System.out.println("User able to login......");
+		System.out.println("Remotly change User able to login......");
+	}
+	public void logout()
+	{
+		System.out.println("Remotly change User able to logout......");
 	}
 	public static void main(String[] args) {
 			System.out.println("Hellow worlds");
